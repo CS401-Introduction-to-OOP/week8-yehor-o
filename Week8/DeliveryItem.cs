e37d15b462d973b34d0d0ea3af3603a4a625d433
Єгор Оводов <yehor.ovodov.25@kse.org.ua>
2026-03-28 14:07:00 +0200

    created Letter class

--- Week8/DeliveryItem.cs
+++ Week8/DeliveryItem.cs
@@ -1,6 +1,21 @@
 namespace Week8;
 
-public class DeliveryItem
+public abstract class DeliveryItem
 {
-    
+    public string TrackingNumber { get; }
+    public double Weight { get; protected set; }
+
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    { 
+        Console.WriteLine($"{TrackingNumber}, {Weight}");
+    }
+
 }
--- Week8/Letter.cs
+++ Week8/Letter.cs
@@ -1,6 +1,12 @@
 namespace Week8;
 
-public class Letter
+public class Letter : DeliveryItem
 {
-    
+    public Letter(string trackingNumber, double weight) : base(trackingNumber, weight)
+    {
+        
+    }
+
+    public override double CalculateCost() => 15 + (Weight * 10);
+
 }
