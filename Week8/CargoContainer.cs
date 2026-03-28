4dcdf8be4ca14ed0acb8603886bf873771b41030
Єгор Оводов <yehor.ovodov.25@kse.org.ua>
2026-03-28 14:20:56 +0200

    created test in program.cs

--- Week8/CargoContainer.cs
+++ Week8/CargoContainer.cs
@@ -1,6 +1,19 @@
 namespace Week8;
 
-public class CargoContainer
+public class CargoContainer<T> where T : DeliveryItem
 {
-    
+    private List<T> _objects = new List<T>();
+
+    public void AddItem(T item) => _objects.Add(item);
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach (T item in _objects)
+        {
+            totalCost += item.CalculateCost();
+        }
+
+        return totalCost;
+    }
 }
--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Week8;
 
-Console.WriteLine("Hello, World!");
+var letter1 = new Letter("153", 15);
+var letter2 = new Letter("415", 5);
+
+var parcel1 = new Parcel("513", 50, "30x10x50");
+var parcel2 = new Parcel("541", 23, "35x15x60");
+
+letter1.PrintInfo();
+parcel2.PrintInfo();
+
+CargoContainer<DeliveryItem> myCargo = new CargoContainer<DeliveryItem>();
+
+myCargo.AddItem(letter1);
+myCargo.AddItem(parcel1);
+myCargo.AddItem(letter2);
+myCargo.AddItem(parcel2);
+
+Console.WriteLine(myCargo.GetTotalCost());
